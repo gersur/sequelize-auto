73f47f6a8f9e978c29a8c630553b58adda2646f4
Gerry Surya <ger_26_6@hotmail.com>
2022-10-27 23:57:57 +0700

    Fix error

--- src/dialects/dialects.ts
+++ src/dialects/dialects.ts
@@ -1,14 +1,37 @@
-import { mssqlOptions } from "./mssql";
-import { mysqlOptions } from "./mysql";
-import { postgresOptions } from "./postgres";
-import { sqliteOptions } from "./sqlite";
-import { DialectOptions } from "./dialect-options";
-import { Dialect } from "sequelize";
+import { mssqlOptions } from './mssql';
+import { mysqlOptions } from './mysql';
+import { postgresOptions } from './postgres';
+import { sqliteOptions } from './sqlite';
+import { DialectOptions } from './dialect-options';
+import { Dialect } from 'sequelize';
+
+const undefinedOptions: DialectOptions = {
+  name: '',
+  hasSchema: false,
+  getForeignKeysQuery: function (tableName: string, schemaName: string): string {
+    throw new Error('Function not implemented.');
+  },
+  countTriggerQuery: function (tableName: string, schemaName: string): string {
+    throw new Error('Function not implemented.');
+  },
+  isPrimaryKey: function (record: any): boolean {
+    throw new Error('Function not implemented.');
+  },
+  isSerialKey: function (record: any): boolean {
+    throw new Error('Function not implemented.');
+  },
+  showViewsQuery: function (schemaName?: string | undefined): string {
+    throw new Error('Function not implemented.');
+  },
+};
 
 export const dialects: { [name in Dialect]: DialectOptions } = {
   mssql: mssqlOptions,
   mysql: mysqlOptions,
   mariadb: mysqlOptions,
   postgres: postgresOptions,
-  sqlite: sqliteOptions
+  sqlite: sqliteOptions,
+  db2: undefinedOptions,
+  snowflake: undefinedOptions,
+  oracle: undefinedOptions,
 };
